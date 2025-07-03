b915188e471fb32a0b8362d38d9796523a9890cb
ShubiDubii <66479732+shubidubi13@users.noreply.github.com>
2025-07-03 05:34:06 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/kukljin-info-brochure/",
+  base: "/kukljin-info-brochure",
   server: {
     host: "::",
     port: 8080,
